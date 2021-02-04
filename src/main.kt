e07f7e0d003d4bdd31e8c59636d7e5804e286fb6
Kelly_qwq <elenanokorystnova1@gmail.com>
2021-02-04 18:56:13 +0700

    123

--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,30 @@
-//fun main() {
-  //  print("Введите скорость первого автомобиля")
-    //var V1 = readLine()!!.toDouble()
-    //print("Введите скорость второго автомобиля")
-    //var V2 = readLine()!!.toDouble()
-    //print("Введите расстояние на момент начала рассчет")
-    //var S_begin = readLine()!!.toDouble()
-    //print("Время прошедшее со старта")
-    //var T = readLine()!!.toDouble()
-    //var S_final = S_begin+V1*T+V2*T
-        //print("Итоговый расчет: $S_final")
+//fun main()
+//{
+  //  print("Введите a: ")
+   // var a = readLine()!!.toDouble()
+  //  print("Введите b: ")
+   // var b = readLine()!!.toDouble()
+   // var average = (a+b)/2
+   // print(average)
+
 //}
-import kotlin.random.Random
 fun main()
 {
 
-    var a = Random.nextInt(from: 1, until:8)
-    print("")
+
+
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
