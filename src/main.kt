77e2ef77e6fb1637766770ffcd3c65076a91344e
CuteDino <rubin4ik5553535@gmail.com>
2021-02-09 19:30:54 +0700

    гоапогаооро

--- src/main.kt
+++ src/main.kt
@@ -10,7 +10,21 @@
 //}
 fun main()
 {
+    val week_days_list = mapOf(
+        1 to "Понедельник",
+        2 to "Вторник",
+        3 to "Среда",
+        4 to "Четверг",
+        5 to "Пятница",
+        6 to "Суббота",
+        7 to "Воскресенье"
+    )
+    print("Введите день года, который хотите узнать: ")
+    var year_days = readLine()!!.toInt()
+    var week_day = 2
+    var user_choise = (year_days+week_day-1)%7
 
+    print("$year_days-ый день: ${week_days_list[user_choise]}\n")
 
 
 
